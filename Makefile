8ef0504704f3743acedc64f2597eba87a854fb48
gitirabassi <giacomo@tirabassi.eu>
2020-08-02 15:37:49 +0200

    new vscode extensions management

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@
 backup:
 	brew bundle dump --force --file=/Users/gitirabassi/.dotfiles/brew/Brewfile
 
+backup-code-extention:
+	code --list-extensions > vscode/extensions.txt
+
 install:
 	bash install.sh
 	brew bundle install --file=brew/Brewfile
--- install.sh
+++ install.sh
@@ -21,32 +21,8 @@ $stow -t $HOME git
 $stow -t $HOME gnupg
 $stow -t $HOME vscode
 $stow -t $HOME myrepos
-$code --install-extension eamodio.gitlens
-$code --install-extension eamodio.remotehub
-$code --install-extension ms-vscode.cpptools
-$code --install-extension vscjava.vscode-java-pack
-$code --install-extension twxs.cmake
-$code --install-extension zxh404.vscode-proto3
-$code --install-extension ms-python.python
-$code --install-extension rust-lang.rust
-$code --install-extension devondcarew.bazel-code
-$code --install-extension timonwong.shellcheck
-$code --install-extension yzhang.markdown-all-in-one
-$code --install-extension joaompinto.asciidoctor-vscode
-$code --install-extension slevesque.vscode-hexdump
-$code --install-extension alefragnani.bookmarks
-$code --install-extension vstirbu.vscode-mermaid-preview
-$code --install-extension ms-vscode-remote.vscode-remote-extensionpack
-$code --install-extension plorefice.devicetree
-$code --install-extension matepek.vscode-catch2-test-adapter
-$code --install-extension adamhartford.vscode-base64
-$code --install-extension bbenoist.vagrant
-$code --install-extension bpruitt-goddard.mermaid-markdown-syntax-highlighting
-$code --install-extension ms-vscode-remote.remote-containers
-$code --install-extension ms-vscode-remote.remote-ssh
-$code --install-extension ms-vscode-remote.remote-ssh-edit
-$code --install-extension ms-vscode-remote.remote-ssh-explorer
-$code --install-extension ms-vscode-remote.remote-wsl
-$code --install-extension redhat.java
-$code --install-extension VisualStudioExptTeam.vscodeintellicode
-$code --install-extension waderyan.gitblame
+
+for ext in (vscode/extensions.txt)
+do
+    $code --install-extension ${ext}
+done
